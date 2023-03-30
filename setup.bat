8658b05ea7d838dc5368be859e44e8413680eef8
DaniruKun <thedanpetrov@gmail.com>
2023-03-30 20:47:30 +0200

    Add requirements.txt

--- setup.bat
+++ setup.bat
@@ -12,23 +12,8 @@ Echo Installing Whisper
 Echo Updating pip
 python.exe -m pip install --upgrade pip
 
-Echo Install PreReqs
-pip install wheel
-pip install setuptools-rust
-pip install git
-
-
-Echo Downloading Whisper
-pip install git+https://github.com/openai/whisper.git 
-
 Echo Installing Requirements...
-pip install numpy
-pip install tqdm
-pip install more-itertools
-pip install ffmpeg-python==0.2.0
-pip install pyaudio
-pip install SpeechRecognition
-pip install prettytable
+pip install -r requirements.txt
 
 :cuda-patch
 Echo Fixing CUDA Since Whisper installs non gpu version.
