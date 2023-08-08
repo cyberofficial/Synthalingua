bb0a463ba68c888b4de8afd00b29b4ab29d125ee
cyber <19499442+cyberofficial@users.noreply.github.com>
2023-08-08 10:03:36 -0400

    * Dynamic Webpage
    * Page will now only load stuff depending on the url parameters

--- modules/api_backend.py
+++ modules/api_backend.py
@@ -3,11 +3,21 @@ import logging
 from flask import Flask, send_from_directory, url_for
 from threading import Thread
 
-header_text = "Welcome to the Test Page"
+header_text = ""
+translated_header_text = ""
+transcribed_header_text = ""
 
 def update_header(new_header):
     global header_text
     header_text = new_header
+
+def update_translated_header(new_header):
+    global translated_header_text
+    translated_header_text = new_header
+
+def update_transcribed_header(new_header):
+    global transcribed_header_text
+    transcribed_header_text = new_header
 
 def flask_server(operation, portnumber):
     if operation == "start":
@@ -55,6 +65,16 @@ def flask_server(operation, portnumber):
         def update_header_route():
             return header_text
 
+        # Route for updating the translated header dynamically
+        @app.route('/update-translated-header')
+        def update_translated_header_route():
+            return translated_header_text
+
+        # Route for updating the transcribed header dynamically
+        @app.route('/update-transcribed-header')
+        def update_transcribed_header_route():
+            return transcribed_header_text
+
         # Generate URL for static file in index.html
         @app.context_processor
         def override_url_for():
@@ -70,7 +90,7 @@ def flask_server(operation, portnumber):
         # Function to run the server
         def run():
             try:
-                app.run(host='localhost', port=port)
+                app.run(host='0.0.0.0', port=port)
             except Exception as e:
                 print(f"Server crashed due to {e}")
                 app.do_teardown_appcontext()
