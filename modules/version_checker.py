b65a4019a6c72c23ed796116fd73013adf7c5901
cyber <19499442+cyberofficial@users.noreply.github.com>
2023-10-27 20:01:12 -0400

    Revert "Regress version"
    This reverts commit 4322e1fc6b52f05b1edef9b33eebb0d3884ba26d.

--- modules/version_checker.py
+++ modules/version_checker.py
@@ -1,6 +1,6 @@
 from modules.imports import *
 
-version = "1.0.9986"
+version = "1.0.9987"
 ScriptCreator = "cyberofficial"
 GitHubRepo = "https://github.com/cyberofficial/Synthalingua"
 repo_owner = "cyberofficial"
