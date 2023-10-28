f8a9d7cf2c75ebf6949135a6cb5dd6492a933829
cyber <19499442+cyberofficial@users.noreply.github.com>
2023-10-27 20:00:39 -0400

    Regress version

--- modules/version_checker.py
+++ modules/version_checker.py
@@ -1,6 +1,6 @@
 from modules.imports import *
 
-version = "1.0.9987"
+version = "1.0.9986"
 ScriptCreator = "cyberofficial"
 GitHubRepo = "https://github.com/cyberofficial/Synthalingua"
 repo_owner = "cyberofficial"
