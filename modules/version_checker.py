b25ecd5495556a7dd11dbc045a7c91ed40789e41
cyber <19499442+cyberofficial@users.noreply.github.com>
2023-11-25 18:01:12 -0500

    Update README.md
    Update README.md
    
    Update README.md
    
    Cookies + Version Bump
    
    Spec Update
    
    Update README.md
    
    Update README.md

--- modules/version_checker.py
+++ modules/version_checker.py
@@ -1,6 +1,6 @@
 from modules.imports import *
 
-version = "1.0.9992"
+version = "1.0.9993"
 ScriptCreator = "cyberofficial"
 GitHubRepo = "https://github.com/cyberofficial/Synthalingua"
 repo_owner = "cyberofficial"
