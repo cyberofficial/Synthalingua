aaf2f02cda80398ed002fd5ee44c95c86d298e75
cyber <19499442+cyberofficial@users.noreply.github.com>
2023-11-26 00:53:32 -0500

    Stream Audio Module Update
    * Added Webserver to Stream Module
    * ReadMe Update
    * Made it so temp file for mic stop being created if microphone not enabled

--- modules/stream_transcription_module.py
+++ modules/stream_transcription_module.py
@@ -3,10 +3,11 @@ from modules.imports import *
 
 # Global shutdown flag
 shutdown_flag = False
+args = parser_args.parse_arguments()
 
-def load_cookies_from_file(file_path):
+def load_cookies_from_file(cookie_file_path):
     cookie_jar = http.cookiejar.MozillaCookieJar()
-    cookie_jar.load(file_path, ignore_discard=True, ignore_expires=True)
+    cookie_jar.load(cookie_file_path, ignore_discard=True, ignore_expires=True)
     return cookie_jar
 
 def start_stream_transcription(hls_url, model_name, temp_dir, segments_max, target_language, stream_language, tasktranslate_task, tasktranscribe_task, webhook_url, cookie_file_path=None):
@@ -50,13 +51,29 @@ def start_stream_transcription(hls_url, model_name, temp_dir, segments_max, targ
             print(f"Error transcribing audio: {e}")
             return ""
 
-    def transcribe_audio(file_path, model):
+    def transcribe_audio(file_path, model, language):
         try:
-            result = model.transcribe(file_path, language=target_language)
+            result = model.transcribe(file_path, language=language)
             return result["text"]
         except RuntimeError as e:
             print(f"Error transcribing audio: {e}")
             return ""
+
+    def detect_language(file_path, model, device=args.device):
+        try:
+            audio = whisper.load_audio(file_path)
+            audio = whisper.pad_or_trim(audio)
+            if args.ram == "12gb":
+                mel = whisper.log_mel_spectrogram(audio, n_mels=128).to(device)
+            else:
+                mel = whisper.log_mel_spectrogram(audio, n_mels=80).to(device)
+            _, language_probs = model.detect_language(mel)
+            detected_language = max(language_probs, key=language_probs.get)
+            return detected_language
+        except RuntimeError as e:
+            print(f"Error detecting language: {e}")
+            detected_language = "n/a"
+            return detected_language
 
     def process_audio_queue(model):
         while True:
@@ -66,6 +83,14 @@ def start_stream_transcription(hls_url, model_name, temp_dir, segments_max, targ
 
             transcription = None
             translation = None
+            if args.stream_original_text:
+                transcription = transcribe_audio(file_path, model, language=None)
+                detected_language = detect_language(file_path, model)
+                print(f"{'-' * 50} {detected_language} Original {'-' * 50}")
+                print(transcription)
+                if args.portnumber:
+                    new_header = f"({detected_language}) {transcription}"
+                    api_backend.update_header(new_header)
 
             if tasktranslate_task:
                 translation = translate_audio(file_path, model)
@@ -74,14 +99,20 @@ def start_stream_transcription(hls_url, model_name, temp_dir, segments_max, targ
                     print(translation)
                     if webhook_url:
                         send_to_discord_webhook(webhook_url, f"Stream EN Translation:\n{translation}\n")
+                    if args.portnumber:
+                        new_header = f"{translation}"
+                        api_backend.update_translated_header(new_header)
 
             if tasktranscribe_task:
-                transcription = transcribe_audio(file_path, model)
+                transcription = transcribe_audio(file_path, model, language=target_language)
                 if transcription:
                     print(f"{'-' * 50} Stream {target_language} Transcription {'-' * 50}")
                     print(transcription)
                     if webhook_url:
                         send_to_discord_webhook(webhook_url, f"Stream {target_language} Transcription:\n{transcription}\n")
+                    if args.portnumber:
+                        new_header = f"{transcription}"
+                        api_backend.update_transcribed_header(new_header)
 
             os.remove(file_path)
 
