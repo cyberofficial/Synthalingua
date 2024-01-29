6093371e2545826f23979ae0553c5e24af236a0a
cyber <19499442+cyberofficial@users.noreply.github.com>
2024-01-29 01:00:20 -0500

    Native API Subtitle
    using native api settings.

--- Synthalingua_Wrapper/subtitlewindow.Designer.vb
+++ Synthalingua_Wrapper/subtitlewindow.Designer.vb
@@ -22,15 +22,171 @@ Partial Class subtitlewindow
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        InfoSaverTimer = New Timer(components)
+        headertextlbl = New Label()
+        translatedheaderlbl = New Label()
+        transcribedheaderlbl = New Label()
+        MenuStrip1 = New MenuStrip()
+        FontSettingsToolStripMenuItem = New ToolStripMenuItem()
+        AddRemoveSubtitleControlToolStripMenuItem = New ToolStripMenuItem()
+        OriginalToolStripMenuItem = New ToolStripMenuItem()
+        ShowToolStripMenuItem = New ToolStripMenuItem()
+        HideToolStripMenuItem = New ToolStripMenuItem()
+        MainTranslationToolStripMenuItem = New ToolStripMenuItem()
+        ShowToolStripMenuItem1 = New ToolStripMenuItem()
+        HideToolStripMenuItem1 = New ToolStripMenuItem()
+        SecondaryTranslationToolStripMenuItem = New ToolStripMenuItem()
+        ShowToolStripMenuItem2 = New ToolStripMenuItem()
+        HideToolStripMenuItem2 = New ToolStripMenuItem()
+        MenuStrip1.SuspendLayout()
         SuspendLayout()
+        ' 
+        ' InfoSaverTimer
+        ' 
+        ' 
+        ' headertextlbl
+        ' 
+        headertextlbl.AutoSize = True
+        headertextlbl.Dock = DockStyle.Top
+        headertextlbl.Location = New Point(0, 24)
+        headertextlbl.Name = "headertextlbl"
+        headertextlbl.Size = New Size(41, 15)
+        headertextlbl.TabIndex = 0
+        headertextlbl.Text = "Label1"
+        ' 
+        ' translatedheaderlbl
+        ' 
+        translatedheaderlbl.AutoSize = True
+        translatedheaderlbl.Dock = DockStyle.Top
+        translatedheaderlbl.Location = New Point(0, 39)
+        translatedheaderlbl.Name = "translatedheaderlbl"
+        translatedheaderlbl.Size = New Size(41, 15)
+        translatedheaderlbl.TabIndex = 1
+        translatedheaderlbl.Text = "Label2"
+        ' 
+        ' transcribedheaderlbl
+        ' 
+        transcribedheaderlbl.AutoSize = True
+        transcribedheaderlbl.Dock = DockStyle.Top
+        transcribedheaderlbl.Location = New Point(0, 54)
+        transcribedheaderlbl.Name = "transcribedheaderlbl"
+        transcribedheaderlbl.Size = New Size(41, 15)
+        transcribedheaderlbl.TabIndex = 2
+        transcribedheaderlbl.Text = "Label3"
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FontSettingsToolStripMenuItem, AddRemoveSubtitleControlToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(475, 24)
+        MenuStrip1.TabIndex = 3
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FontSettingsToolStripMenuItem
+        ' 
+        FontSettingsToolStripMenuItem.Name = "FontSettingsToolStripMenuItem"
+        FontSettingsToolStripMenuItem.Size = New Size(88, 20)
+        FontSettingsToolStripMenuItem.Text = "Font Settings"
+        ' 
+        ' AddRemoveSubtitleControlToolStripMenuItem
+        ' 
+        AddRemoveSubtitleControlToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {OriginalToolStripMenuItem, MainTranslationToolStripMenuItem, SecondaryTranslationToolStripMenuItem})
+        AddRemoveSubtitleControlToolStripMenuItem.Name = "AddRemoveSubtitleControlToolStripMenuItem"
+        AddRemoveSubtitleControlToolStripMenuItem.Size = New Size(175, 20)
+        AddRemoveSubtitleControlToolStripMenuItem.Text = "Add/Remove Subtitle Control"
+        ' 
+        ' OriginalToolStripMenuItem
+        ' 
+        OriginalToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ShowToolStripMenuItem, HideToolStripMenuItem})
+        OriginalToolStripMenuItem.Name = "OriginalToolStripMenuItem"
+        OriginalToolStripMenuItem.Size = New Size(189, 22)
+        OriginalToolStripMenuItem.Text = "Original"
+        ' 
+        ' ShowToolStripMenuItem
+        ' 
+        ShowToolStripMenuItem.Name = "ShowToolStripMenuItem"
+        ShowToolStripMenuItem.Size = New Size(103, 22)
+        ShowToolStripMenuItem.Text = "Show"
+        ' 
+        ' HideToolStripMenuItem
+        ' 
+        HideToolStripMenuItem.Name = "HideToolStripMenuItem"
+        HideToolStripMenuItem.Size = New Size(103, 22)
+        HideToolStripMenuItem.Text = "Hide"
+        ' 
+        ' MainTranslationToolStripMenuItem
+        ' 
+        MainTranslationToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ShowToolStripMenuItem1, HideToolStripMenuItem1})
+        MainTranslationToolStripMenuItem.Name = "MainTranslationToolStripMenuItem"
+        MainTranslationToolStripMenuItem.Size = New Size(189, 22)
+        MainTranslationToolStripMenuItem.Text = "Main Translation"
+        ' 
+        ' ShowToolStripMenuItem1
+        ' 
+        ShowToolStripMenuItem1.Name = "ShowToolStripMenuItem1"
+        ShowToolStripMenuItem1.Size = New Size(103, 22)
+        ShowToolStripMenuItem1.Text = "Show"
+        ' 
+        ' HideToolStripMenuItem1
+        ' 
+        HideToolStripMenuItem1.Name = "HideToolStripMenuItem1"
+        HideToolStripMenuItem1.Size = New Size(103, 22)
+        HideToolStripMenuItem1.Text = "Hide"
+        ' 
+        ' SecondaryTranslationToolStripMenuItem
+        ' 
+        SecondaryTranslationToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ShowToolStripMenuItem2, HideToolStripMenuItem2})
+        SecondaryTranslationToolStripMenuItem.Name = "SecondaryTranslationToolStripMenuItem"
+        SecondaryTranslationToolStripMenuItem.Size = New Size(189, 22)
+        SecondaryTranslationToolStripMenuItem.Text = "Secondary Translation"
+        ' 
+        ' ShowToolStripMenuItem2
+        ' 
+        ShowToolStripMenuItem2.Name = "ShowToolStripMenuItem2"
+        ShowToolStripMenuItem2.Size = New Size(103, 22)
+        ShowToolStripMenuItem2.Text = "Show"
+        ' 
+        ' HideToolStripMenuItem2
+        ' 
+        HideToolStripMenuItem2.Name = "HideToolStripMenuItem2"
+        HideToolStripMenuItem2.Size = New Size(103, 22)
+        HideToolStripMenuItem2.Text = "Hide"
         ' 
         ' subtitlewindow
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(475, 175)
+        Controls.Add(transcribedheaderlbl)
+        Controls.Add(translatedheaderlbl)
+        Controls.Add(headertextlbl)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
         Name = "subtitlewindow"
+        StartPosition = FormStartPosition.CenterScreen
         Text = "subtitlewindow"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents InfoSaverTimer As Timer
+    Friend WithEvents headertextlbl As Label
+    Friend WithEvents translatedheaderlbl As Label
+    Friend WithEvents transcribedheaderlbl As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FontSettingsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddRemoveSubtitleControlToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OriginalToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ShowToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HideToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MainTranslationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ShowToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents HideToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents SecondaryTranslationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ShowToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents HideToolStripMenuItem2 As ToolStripMenuItem
 End Class
--- Synthalingua_Wrapper/subtitlewindow.vb
+++ Synthalingua_Wrapper/subtitlewindow.vb
@@ -1,31 +1,99 @@
-﻿Imports CefSharp.WinForms
-Imports CefSharp
+﻿Imports System.Net.Http
+Imports System.Net.Sockets
+Imports System.Threading
 
 Public Class subtitlewindow
+    Private httpClient As HttpClient
+    Private cts As CancellationTokenSource
+    Private Const URLHeader As String = "http://localhost:2000/update-header"
+    Private Const URLTranslatedHeader As String = "http://localhost:2000/update-translated-header"
+    Private Const URLTranscribedHeader As String = "http://localhost:2000/update-transcribed-header"
 
-    Private WithEvents browser As ChromiumWebBrowser
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        httpClient = New HttpClient()
+        cts = New CancellationTokenSource()
+    End Sub
+
     Private Sub subtitlewindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Set CefSharp settings and cache path
-        Dim settings As New CefSettings()
-        settings.CachePath = Application.StartupPath & "\Cache"
-
-        ' Add process exe path to sub processes
-        settings.BrowserSubprocessPath = Application.StartupPath & "\CefSharp.BrowserSubprocess.exe"
-
-        ' Initialize CefSharp with the settings
-        CefSharp.Cef.Initialize(settings)
-
-        ' Create a new browser instance
-        browser = New ChromiumWebBrowser("http://example.com")
-
-        ' Dock the browser in the main form
-        browser.Dock = DockStyle.Fill
-        Me.Controls.Add(browser)
+        InfoSaverTimer.Interval = 50 ' Set your timer interval to 50ms
+        InfoSaverTimer.Start()
     End Sub
 
     Private Sub subtitlewindow_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        browser.Dispose()
-        CefSharp.Cef.Shutdown()
-        Cef.Shutdown()
+        InfoSaverTimer.Stop()
+        cts.Cancel() ' Cancel any ongoing operations
+        httpClient.Dispose()
+        cts.Dispose() ' Dispose of the CancellationTokenSource
     End Sub
+
+    Private Async Sub InfoSaverTimer_Tick(sender As Object, e As EventArgs) Handles InfoSaverTimer.Tick
+        ' Check if the form is closing. If so, do not proceed with updates.
+        If Me.IsDisposed OrElse Me.Disposing Then
+            Return
+        End If
+
+        Dim headerText As String = String.Empty
+        Dim translatedHeaderText As String = String.Empty
+        Dim transcribedHeaderText As String = String.Empty
+
+        Try
+            headerText = Await FetchTextFromUrl(URLHeader, cts.Token)
+            Debug.WriteLine("Header Text: " & headerText)
+        Catch ex As Exception
+            Debug.WriteLine("Error fetching header text: " & ex.Message)
+        End Try
+
+        Try
+            translatedHeaderText = Await FetchTextFromUrl(URLTranslatedHeader, cts.Token)
+            Debug.WriteLine("Translated Header Text: " & translatedHeaderText)
+        Catch ex As Exception
+            Debug.WriteLine("Error fetching translated header text: " & ex.Message)
+        End Try
+
+        Try
+            transcribedHeaderText = Await FetchTextFromUrl(URLTranscribedHeader, cts.Token)
+            Debug.WriteLine("Transcribed Header Text: " & transcribedHeaderText)
+        Catch ex As Exception
+            Debug.WriteLine("Error fetching transcribed header text: " & ex.Message)
+        End Try
+
+        ' Before updating the UI, check if the form is still open and its handle is created
+        If Not Me.IsDisposed AndAlso Not Me.Disposing AndAlso Me.IsHandleCreated Then
+            Try
+                Me.Invoke(Sub()
+                              headertextlbl.Text = headerText
+                              translatedheaderlbl.Text = translatedHeaderText
+                              transcribedheaderlbl.Text = transcribedHeaderText
+                          End Sub)
+            Catch ex As InvalidOperationException
+                Debug.WriteLine("InvalidOperationException: " & ex.Message)
+            Catch ex As Exception
+                Debug.WriteLine("General Exception: " & ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Async Function FetchTextFromUrl(url As String, ct As CancellationToken) As Task(Of String)
+        Try
+            ' Directly returning the response string as it's just plain text
+            Return Await httpClient.GetStringAsync(url, ct)
+        Catch ex As OperationCanceledException
+            Debug.WriteLine("Operation canceled")
+            Return "Operation canceled"
+        Catch ex As HttpRequestException
+            Debug.WriteLine("HttpRequestException: " & ex.Message)
+            Return "Error: " & ex.Message
+        Catch ex As SocketException
+            Debug.WriteLine("SocketException: " & ex.Message)
+            Return "Error: " & ex.Message
+        Catch ex As Exception
+            Debug.WriteLine("General Exception: " & ex.Message)
+            Return "Error: " & ex.Message
+        End Try
+    End Function
+
 End Class
