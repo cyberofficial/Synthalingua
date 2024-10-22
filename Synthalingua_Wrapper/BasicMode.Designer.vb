2ce0a6c15049f36d025ad33d0a0081b69e825a8b
cyber <19499442+cyberofficial@users.noreply.github.com>
2024-10-22 16:59:45 -0400

    UI Changes
    * UI Changes for Basic Mode

---
+++ Synthalingua_Wrapper/BasicMode.Designer.vb
@@ -0,0 +1,325 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BasicMode
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        ComboBox1 = New ComboBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        ComboBox2 = New ComboBox()
+        ComboBox3 = New ComboBox()
+        Label4 = New Label()
+        ComboBox4 = New ComboBox()
+        Label5 = New Label()
+        ComboBox5 = New ComboBox()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        ComboBox6 = New ComboBox()
+        Label9 = New Label()
+        Label10 = New Label()
+        LinkLabel1 = New LinkLabel()
+        Label11 = New Label()
+        DiscordWebHookBox = New TextBox()
+        DiscordWebHookBoxShowURLButton = New Button()
+        Label12 = New Label()
+        ComboBox7 = New ComboBox()
+        Label13 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(121, 37)
+        Label1.TabIndex = 0
+        Label1.Text = "I want to"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"translate", "transcribe", "make captions"})
+        ComboBox1.Location = New Point(139, 6)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(210, 45)
+        ComboBox1.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(355, 9)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 37)
+        Label2.TabIndex = 2
+        Label2.Text = "with a"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(685, 9)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(102, 37)
+        Label3.TabIndex = 4
+        Label3.Text = "using a"
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Items.AddRange(New Object() {"live video feed", "microphone", "file on my pc"})
+        ComboBox2.Location = New Point(793, 6)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(256, 45)
+        ComboBox2.TabIndex = 5
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Items.AddRange(New Object() {"live stream", "microphone", "file from my pc"})
+        ComboBox3.Location = New Point(450, 6)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(229, 45)
+        ComboBox3.TabIndex = 6
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(12, 79)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(223, 37)
+        Label4.TabIndex = 7
+        Label4.Text = "I will be using my"
+        ' 
+        ' ComboBox4
+        ' 
+        ComboBox4.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox4.FormattingEnabled = True
+        ComboBox4.Items.AddRange(New Object() {"Graphics Card", "CPU"})
+        ComboBox4.Location = New Point(241, 76)
+        ComboBox4.Name = "ComboBox4"
+        ComboBox4.Size = New Size(237, 45)
+        ComboBox4.TabIndex = 8
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(484, 79)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(218, 37)
+        Label5.TabIndex = 9
+        Label5.Text = "and will be using"
+        ' 
+        ' ComboBox5
+        ' 
+        ComboBox5.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox5.FormattingEnabled = True
+        ComboBox5.Items.AddRange(New Object() {"1GB", "2GB", "4GB", "6GB", "12GB"})
+        ComboBox5.Location = New Point(708, 76)
+        ComboBox5.Name = "ComboBox5"
+        ComboBox5.Size = New Size(134, 45)
+        ComboBox5.TabIndex = 10
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(848, 79)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(180, 37)
+        Label6.TabIndex = 11
+        Label6.Text = "RAM from my"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(1021, 79)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(194, 37)
+        Label7.TabIndex = 12
+        Label7.Text = "<device type>."
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(12, 135)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(24, 37)
+        Label8.TabIndex = 13
+        Label8.Text = "I"
+        ' 
+        ' ComboBox6
+        ' 
+        ComboBox6.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox6.FormattingEnabled = True
+        ComboBox6.Items.AddRange(New Object() {"DO", "DO NOT"})
+        ComboBox6.Location = New Point(42, 132)
+        ComboBox6.Name = "ComboBox6"
+        ComboBox6.Size = New Size(121, 45)
+        ComboBox6.TabIndex = 14
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(169, 135)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(254, 37)
+        Label9.TabIndex = 15
+        Label9.Text = "want to use discord."
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(413, 135)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(151, 37)
+        Label10.TabIndex = 16
+        Label10.Text = "My Discord"
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Location = New Point(559, 135)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(228, 37)
+        LinkLabel1.TabIndex = 17
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Discord Webhook"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(779, 135)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(73, 37)
+        Label11.TabIndex = 18
+        Label11.Text = "url is"
+        ' 
+        ' DiscordWebHookBox
+        ' 
+        DiscordWebHookBox.Location = New Point(858, 132)
+        DiscordWebHookBox.Name = "DiscordWebHookBox"
+        DiscordWebHookBox.PlaceholderText = "Place Webhook here. Click the circle to show the text."
+        DiscordWebHookBox.Size = New Size(684, 43)
+        DiscordWebHookBox.TabIndex = 19
+        DiscordWebHookBox.UseSystemPasswordChar = True
+        ' 
+        ' DiscordWebHookBoxShowURLButton
+        ' 
+        DiscordWebHookBoxShowURLButton.Location = New Point(1548, 132)
+        DiscordWebHookBoxShowURLButton.Name = "DiscordWebHookBoxShowURLButton"
+        DiscordWebHookBoxShowURLButton.Size = New Size(49, 43)
+        DiscordWebHookBoxShowURLButton.TabIndex = 20
+        DiscordWebHookBoxShowURLButton.Text = "👁️"
+        DiscordWebHookBoxShowURLButton.UseVisualStyleBackColor = True
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(12, 180)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(168, 37)
+        Label12.TabIndex = 21
+        Label12.Text = "I want to use"
+        ' 
+        ' ComboBox7
+        ' 
+        ComboBox7.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox7.FormattingEnabled = True
+        ComboBox7.Items.AddRange(New Object() {"a caption window", "my webbrowser"})
+        ComboBox7.Location = New Point(186, 177)
+        ComboBox7.Name = "ComboBox7"
+        ComboBox7.Size = New Size(292, 45)
+        ComboBox7.TabIndex = 22
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Location = New Point(484, 180)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(305, 37)
+        Label13.TabIndex = 23
+        Label13.Text = "to watch the live stream."
+        ' 
+        ' BasicMode
+        ' 
+        AutoScaleDimensions = New SizeF(15F, 37F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1617, 422)
+        Controls.Add(Label13)
+        Controls.Add(ComboBox7)
+        Controls.Add(Label12)
+        Controls.Add(DiscordWebHookBoxShowURLButton)
+        Controls.Add(DiscordWebHookBox)
+        Controls.Add(Label11)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(ComboBox6)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(ComboBox5)
+        Controls.Add(Label5)
+        Controls.Add(ComboBox4)
+        Controls.Add(Label4)
+        Controls.Add(ComboBox3)
+        Controls.Add(ComboBox2)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label1)
+        Font = New Font("Segoe UI", 20F)
+        Margin = New Padding(6, 7, 6, 7)
+        Name = "BasicMode"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "BasicMode"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents ComboBox6 As ComboBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label11 As Label
+    Friend WithEvents DiscordWebHookBox As TextBox
+    Friend WithEvents DiscordWebHookBoxShowURLButton As Button
+    Friend WithEvents Label12 As Label
+    Friend WithEvents ComboBox7 As ComboBox
+    Friend WithEvents Label13 As Label
+End Class
--- Synthalingua_Wrapper/UserMode.vb
+++ Synthalingua_Wrapper/UserMode.vb
@@ -3,4 +3,9 @@
         MainUI.Show()
         Me.Close()
     End Sub
+
+    Private Sub SimpleBtn_Click(sender As Object, e As EventArgs) Handles SimpleBtn.Click
+        BasicMode.Show()
+        Me.Close()
+    End Sub
 End Class
