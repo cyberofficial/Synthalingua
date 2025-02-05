bdaf70184f74e281224199bd124f8ab40eafe87e
Cyber Official <19499442+cyberofficial@users.noreply.github.com>
2025-02-05 10:27:44 -0500

    UI Update

--- Synthalingua_Wrapper/My Project/Application.Designer.vb
+++ Synthalingua_Wrapper/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Synthalingua_Wrapper.UserMode
+            Me.MainForm = Global.Synthalingua_Wrapper.MainUI
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
