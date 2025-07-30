c2af5005e5af9ed2798d248466a8af36e22c7b8a
YuumiPie <altchanjeremy08@gmail.com>
2025-07-30 03:36:42 -0700

    Set Whisper Models
    OpenVINO detect_language() currently broken

---
+++ modules/models/BaseWhisper.py
@@ -0,0 +1,56 @@
+import os
+import whisper
+
+
+class BaseWhisperModel:
+    def __init__(self, model: str, device, download_root: str) -> None:
+        download_root = os.path.join(download_root, "Whisper")
+
+        self.model = model
+        self.device = device
+        self.audio_model = whisper.load_model(model, device=device, download_root=download_root)
+
+    def detect_language(self, file_path: str) -> dict:
+        """
+        Detects language from audio content
+
+        Args:
+            file_path (str): Path to the audio file
+
+        Returns:
+            dict[str, float]: List of languages with their percentage likelihood of being the language of the audio
+        """
+        audio = whisper.load_audio(file_path)
+        audio = whisper.pad_or_trim(audio)
+
+        # similarly (if ram == "11gb-v3")
+        if self.model == "large-v3":
+            mel = whisper.log_mel_spectrogram(audio, n_mels=128).to(self.device)
+        else:
+            mel = whisper.log_mel_spectrogram(audio, n_mels=80).to(self.device)
+        _, language_probs = self.audio_model.detect_language(mel)
+        return language_probs
+
+    def transcribe(self, file_path: str, **kwargs) -> str:
+        """
+        Transcribe an audio file
+
+        Args:
+            file_path (str): Path to the audio file to transcribe
+
+            kwargs:
+                language (str): Language code for transcription
+                fp16 (bool): Whether to use 16-bit precision
+                condition_on_previous_text (bool): Provides the previous output of the model as a prompt for the next window
+                task (str): Perform X->X speech recognition ('transcribe') or X->English translation ('translate')
+
+        Returns:
+            str: string of the transcription
+        """
+        language = kwargs.get("language")
+        fp16 = kwargs.get("fp16")
+        condition_on_previous_text = kwargs.get("condition_on_previous_text")
+        task = kwargs.get("task")
+
+        result = self.audio_model.transcribe(audio=file_path, language=language, fp16=fp16, condition_on_previous_text=condition_on_previous_text, task=task)
+        return result["text"]
---
+++ modules/models/FasterWhisper.py
@@ -0,0 +1,67 @@
+import os
+import faster_whisper
+import pycountry
+import whisper
+from faster_whisper import WhisperModel
+
+
+class FasterWhisperModel:
+    def __init__(self, model: str, device, download_root: str, compute_type: str) -> None:
+        download_root = os.path.join(download_root, "FasterWhisper")
+
+        self.model = model
+        self.device = device
+        self.audio_model = WhisperModel(model, device=device, download_root=download_root, compute_type=compute_type)
+
+    def detect_language(self, file_path: str) -> dict:
+        """
+        Detects language from audio content
+
+        Args:
+            file_path (str): Path to the audio file to transcribe
+
+        Returns:
+            dict[str, float]: List of languages with their percentage likelihood of being the language of the audio
+        """
+        audio = faster_whisper.audio.decode_audio(file_path)
+        audio = faster_whisper.audio.pad_or_trim(audio)
+
+        # Could not find log_mel_spectrogram() method in faster_whisper library
+        # Whisper library's implementation replacement
+        if self.model == "large-v3":
+            mel = whisper.log_mel_spectrogram(audio, n_mels=128).to(self.device)
+        else:
+            mel = whisper.log_mel_spectrogram(audio, n_mels=80).to(self.device)
+
+        _, _, language_probs = self.audio_model.detect_language(features=mel)
+        return dict(language_probs)
+
+    def transcribe(self, file_path: str, **kwargs) -> str:
+        """
+        Transcribe an audio file
+
+        Args:
+            file_path (str): Path to the audio file to transcribe
+
+            kwargs:
+                language (str): Language code for transcription
+                fp16 (bool): Whether to use 16-bit precision
+                condition_on_previous_text (bool): Provides the previous output of the model as a prompt for the next window
+                task (str): Perform X->X speech recognition ('transcribe') or X->English translation ('translate')
+
+        Returns:
+            str: string of the transcription
+        """
+        language = kwargs.get("language")
+        condition_on_previous_text = kwargs.get("condition_on_previous_text")
+        task = kwargs.get("task")
+
+        # Pycountry library used to convert language to the required format (English -> en)
+        language = pycountry.languages.get(language).alpha_2
+        segments, _ = self.audio_model.transcribe(audio=file_path, task=task, condition_on_previous_text=condition_on_previous_text, language=language)
+
+        result = ""
+        for segment in segments:
+            result += segment
+        # First segment starts with a space (ex " text text text...") strip() used to remove it
+        return result.strip()
