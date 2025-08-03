6f136c09c074e1b970e296eea6c6a08e435a0f42
Cyber Official <19499442+cyberofficial@users.noreply.github.com>
2025-08-03 14:31:45 -0400

    It built, but also didn't. mfw forgor build lines. End my suffering plz.

--- build.bat
+++ build.bat
@@ -42,10 +42,13 @@ python -m nuitka --standalone ^
     --windows-console-mode=force ^
     --include-package=whisper ^
     --include-package-data=whisper ^
+    --include-distribution-metadata=whisper ^
     --include-package=openvino ^
     --include-package-data=openvino ^
+    --include-distribution-metadata=openvino ^
     --include-package=librosa ^
     --include-package-data=librosa ^
+    --include-distribution-metadata=librosa ^
     --include-module=modules.transcribe_worker ^
     --include-data-file=modules/transcribe_worker.py=modules/transcribe_worker.py ^
     --include-data-dir=html_data=html_data ^
@@ -62,12 +65,14 @@ python -m nuitka --standalone ^
     --output-dir="E:\Synthalingua\Synthalingua_Main\dist\main_release" ^
     --include-data-dir="E:\Synthalingua\Synthalingua_Main\data_whisper\Lib\site-packages\faster_whisper"=faster_whisper ^
     --include-data-dir="E:\Synthalingua\Synthalingua_Main\data_whisper\Lib\site-packages\optimum"=optimum ^
-    --low-memory ^
     --nofollow-import-to=yt_dlp.lazy_extractors ^
     --lto=no ^
+    --mingw64 ^
+    --clang ^
+    --show-progress ^
     synthalingua.py
 
-
+    --low-memory ^
 goto :eof
 ::    --include-package=demucs ^
 ::    --include-package-data=demucs ^
